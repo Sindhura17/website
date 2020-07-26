b58de60f535ce879196a41a1178c5c486cac1343
sindhura17 <sindhura.l.bhat@gmail.com>
2020-07-26 12:36:51 +0000

    upload

--- sj/workapp/upload.py
+++ sj/workapp/upload.py
@@ -6,7 +6,7 @@ class Facebook:
         @staticmethod
         def get_access_token():
                 res={}
-                access_token = os.getcwd()+'\\access_tokens.sh'
+                access_token = os.getcwd()+'/website/sj/access_tokens.sh'
                 f = open(access_token, 'r+')
                 lines = f.readlines()
                 for line in lines:
@@ -15,12 +15,12 @@ class Facebook:
                                 res['FACEBOOK_PAGE_ID']=tokens[1].strip()
                         elif tokens[0] == 'FACEBOOK_PAGE_ACCESS_TOKEN':
                                 res['FACEBOOK_PAGE_ACCESS_TOKEN']=tokens[1].strip()
-                return res     
+                return res
         def __init__(self):
                 token=self.get_access_token()
                 self.page_id=token['FACEBOOK_PAGE_ID']
                 self.page_access_token=token['FACEBOOK_PAGE_ACCESS_TOKEN']
-                
+
         def publish_photo_msg(self,msg, image_url):
                 graph = facebook.GraphAPI(self.page_access_token)
                 photo = open(image_url, "rb")
@@ -32,5 +32,5 @@ class Facebook:
                 "caption":message,
                 }
                 requests.post(url,params)
-                return''' 
+                return'''
 
